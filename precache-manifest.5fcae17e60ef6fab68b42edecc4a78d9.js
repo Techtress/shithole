69110141ea7200ebf2e2dae3590ab5540cfc1391
Techtress <47042355+Techtress@users.noreply.github.com>
2020-06-09 03:46:14 -0400

    Updates

---
+++ precache-manifest.5fcae17e60ef6fab68b42edecc4a78d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a5da28a2e0380053fcb332957372d7",
+    "url": "/shithole/index.html"
+  },
+  {
+    "revision": "31e23ae84f5a6fcb6be7",
+    "url": "/shithole/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "765e4bf40d213504291e",
+    "url": "/shithole/static/js/2.9e565700.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shithole/static/js/2.9e565700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e23ae84f5a6fcb6be7",
+    "url": "/shithole/static/js/main.48ef3c3c.chunk.js"
+  },
+  {
+    "revision": "79d22bff8b32f435bf16",
+    "url": "/shithole/static/js/runtime-main.48bad719.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shithole/static/media/logo.5d5d9eef.svg"
+  }
+]);
